69d6f1eb94d0eae0706510c4235f0f388e9a20e8
陈松 <chensong@weidian.com>
2026-08-20 13:33:46 +0800

    add ipam usage

--- function_collector/func_ipam.py
+++ function_collector/func_ipam.py
@@ -59,6 +59,27 @@ def get_network_address_tree(data):
         new_item["end"] = item["end_ip"]
         new_item["children"] = []
 
+        # 计算使用率
+        start_ip = int(item["start_ip"])
+        end_ip = int(item["end_ip"])
+        total_ips = end_ip - start_ip - 1  # 排除网络地址和广播地址
+
+        if total_ips > 0:
+            # 查询该网段内已使用的IP数量
+            query_data = {
+                "start_ip": start_ip,
+                "end_ip": end_ip
+            }
+            used_ips = db.get_ipaddr_list(query_data)
+            if used_ips != "failed" and isinstance(used_ips, list):
+                used_count = len(used_ips)
+                used_per = round((used_count / total_ips) * 100, 2)
+                new_item["used_per"] = str(used_per)
+            else:
+                new_item["used_per"] = "0"
+        else:
+            new_item["used_per"] = "0"
+
         tree_stack = [tree]
 
         while len(tree_stack) > 0:
